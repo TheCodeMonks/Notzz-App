2b40d036c2c9e8c174c66452ba708f215fea9bd5
Sanju S <spikeysanju98@gmail.com>
2020-11-19 08:39:11 +0530

    Fix note icon on dark mode
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/thecodemonks/org/nottzapp/datastore/UIModePreference.kt
+++ app/src/main/java/thecodemonks/org/nottzapp/datastore/UIModePreference.kt
@@ -31,11 +31,11 @@ package thecodemonks.org.nottzapp.datastore
 
 
 import android.content.Context
-import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
