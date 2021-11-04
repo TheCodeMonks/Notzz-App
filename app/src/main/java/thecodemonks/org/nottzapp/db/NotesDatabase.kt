ec22e513e2e8a46f7c8b3fbe595cb3e33fe8285a
Sanju S <spikeysanju98@gmail.com>
2021-11-05 01:06:25 +0530

    Migrate from `groovy` to `kts` #182
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/thecodemonks/org/nottzapp/db/NotesDatabase.kt
+++ app/src/main/java/thecodemonks/org/nottzapp/db/NotesDatabase.kt
@@ -29,9 +29,7 @@
 
 package thecodemonks.org.nottzapp.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import thecodemonks.org.nottzapp.model.Notes
 
@@ -43,23 +41,4 @@ import thecodemonks.org.nottzapp.model.Notes
 
 abstract class NotesDatabase : RoomDatabase() {
     abstract fun getNotesDao(): NotesDao
-
-    companion object {
-        @Volatile
-        private var instance: NotesDatabase? = null
-        private val LOCK = Any()
-
-        // Check for DB instance if not null then get or insert or else create new DB Instance
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-
-            instance ?: createDatabase(context).also { instance = it }
-        }
-
-        // create db instance
-        private fun createDatabase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            NotesDatabase::class.java,
-            "notes_db.db"
-        ).build()
-    }
 }
